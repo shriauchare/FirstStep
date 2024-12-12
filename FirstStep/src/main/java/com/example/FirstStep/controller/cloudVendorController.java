31e52349837d9d9cbfd5911303491074468947ec
shrip <shrip@DESKTOP-MM79U6J>
2024-12-13 02:03:38 +0530

    Second

---
+++ FirstStep/src/main/java/com/example/FirstStep/controller/cloudVendorController.java
@@ -0,0 +1,12 @@
+package com.example.FirstStep.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class cloudVendorController {
+	
+	
+
+}
---
+++ FirstStep/src/main/java/com/example/FirstStep/service/CloudVendorService.java
@@ -0,0 +1,5 @@
+package com.example.FirstStep.service;
+
+public interface CloudVendorService {
+
+}
